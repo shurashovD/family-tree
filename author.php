5b10c86df3ff337acc20546bad5306df68aaa379
shurashovD <78542938+shurashovD@users.noreply.github.com>
2021-07-16 17:28:43 +0300

    author.php проверяет права

--- author.php
+++ author.php
@@ -1,5 +1,12 @@
 <?php
+    require_once('db.php');
+    $db = new Db();
+    $link = $db->db_connect();
+    
     session_start();
     if ( isset($_SESSION['user_id']) ) {
         $user = $_SESSION['user_id'];
+        $query = "SELECT `user_rights` from `users` WHERE `user_id`='$user'";
+        $result = mysqli_query($link, $query);
+        $_SESSION['user_rights'] = mysqli_fetch_all($result, MYSQLI_ASSOC)[0]['user_rights'];
     }
